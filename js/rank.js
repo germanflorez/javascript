0a5113f4f55e809fe58059687e78b1b55f56cf22
Germanflorez <german.andres.florez@gmail.com>
2020-06-21 22:15:25 +0200

    Corrección errores

--- js/rank.js
+++ js/rank.js
@@ -1,117 +1,67 @@
 //Exercici Rating
-
-
 function cambiarIcono(id) {
     if (id=="face1") {  
-        document.getElementById("face1").style.color="#ffffff"; 
-        document.getElementById("face1").style.transition = "color 0.2s linear 0.1s";
+        document.getElementById("face1").className += " colorFace";           
     }  
     if (id=="face2") {
-        document.getElementById("face1").style.color="#ffffff";
-        document.getElementById("face1").style.transition = "color 0.2s linear 0.1s"; 
-        document.getElementById("face2").style.color="#ffffff"; 
-        document.getElementById("face2").style.transition = "color 0.2s linear 0.1s";
+        document.getElementById("face1").className += " colorFace";
+        document.getElementById("face2").className += " colorFace";   
     }
     if (id=="face3") {
-        document.getElementById("face1").style.color="#ffffff";
-        document.getElementById("face1").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face2").style.color="#ffffff"; 
-        document.getElementById("face2").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face3").style.color="#ffffff";
-        document.getElementById("face3").style.transition = "color 0.2s linear 0.1s";  
+        document.getElementById("face1").className += " colorFace";
+        document.getElementById("face2").className += " colorFace";   
+        document.getElementById("face3").className += " colorFace";
     }
     if (id=="face4") {
-        document.getElementById("face1").style.color="#ffffff";
-        document.getElementById("face1").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face2").style.color="#ffffff"; 
-        document.getElementById("face2").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face3").style.color="#ffffff";
-        document.getElementById("face3").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face4").style.color="#ffffff";
-        document.getElementById("face4").style.transition = "color 0.2s linear 0.1s";  
+        document.getElementById("face1").className += " colorFace";
+        document.getElementById("face2").className += " colorFace";   
+        document.getElementById("face3").className += " colorFace";
+        document.getElementById("face4").className += " colorFace";
     }
     if (id=="face5") {
-        document.getElementById("face1").style.color="#ffffff";
-        document.getElementById("face1").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face2").style.color="#ffffff"; 
-        document.getElementById("face2").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face3").style.color="#ffffff";
-        document.getElementById("face3").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face4").style.color="#ffffff";
-        document.getElementById("face4").style.transition = "color 0.2s linear 0.1s";
-        document.getElementById("face5").style.color="#ffffff";
-        document.getElementById("face5").style.transition = "color 0.2s linear 0.1s"; 
+        document.getElementById("face1").className += " colorFace";
+        document.getElementById("face2").className += " colorFace";   
+        document.getElementById("face3").className += " colorFace";
+        document.getElementById("face4").className += " colorFace";
+        document.getElementById("face5").className += " colorFace";
+    }    
+}
+function devolverColor(id) {
+     if (id=="face1") {  
+        document.getElementById("face1").className = " d-inline";           
+    }  
+    if (id=="face2") {
+        document.getElementById("face1").className = " d-inline";
+        document.getElementById("face2").className = " d-inline";   
+    }
+    if (id=="face3") {
+        document.getElementById("face1").className = " d-inline";
+        document.getElementById("face2").className = " d-inline";   
+        document.getElementById("face3").className = " d-inline";
+    }
+    if (id=="face4") {
+        document.getElementById("face1").className = " d-inline";
+        document.getElementById("face2").className = " d-inline";   
+        document.getElementById("face3").className = " d-inline";
+        document.getElementById("face4").className = " d-inline";
+    }
+    if (id=="face5") {
+        document.getElementById("face1").className = " d-inline";
+        document.getElementById("face2").className = " d-inline";   
+        document.getElementById("face3").className = " d-inline";
+        document.getElementById("face4").className = " d-inline";
+        document.getElementById("face5").className = " d-inline";
     }   
 }
-
-
-
-
-function devolverColor(id) {
-    if (id=="face1") {
-        document.getElementById("face1").style.color="#000000";
-    }
-    if (id=="face2") {
-        document.getElementById("face1").style.color="#000000"; 
-        document.getElementById("face2").style.color="#000000";  
-    }
-    if (id=="face3") {
-        document.getElementById("face1").style.color="#000000"; 
-        document.getElementById("face2").style.color="#000000"; 
-        document.getElementById("face3").style.color="#000000"; 
-    }
-    if (id=="face4") {
-        document.getElementById("face1").style.color="#000000"; 
-        document.getElementById("face2").style.color="#000000"; 
-        document.getElementById("face3").style.color="#000000";
-        document.getElementById("face4").style.color="#000000"; 
-    }
-    if (id=="face5") {
-        document.getElementById("face1").style.color="#000000"; 
-        document.getElementById("face2").style.color="#000000"; 
-        document.getElementById("face3").style.color="#000000";
-        document.getElementById("face4").style.color="#000000"; 
-        document.getElementById("face5").style.color="#000000";
-    }
-}
-
-var f1=0, f2=0, f3=0, f4=0, f5=0;
+var puntuacion =[0,0,0,0,0];
 var total = 0;
 function califica(id) { 
-    if(id=="face1"){
-        /* var item = document.createElement("LI");
-        var text = document.createTextNode(" * ");
-        item.appendChild(text);
-        document.getElementById("rank").appendChild(item);  */
-        f1 += 1; 
-        total += 1; 
-        document.getElementById("f1").innerHTML =  f1;  
+    for(var i = 0; i <= puntuacion.length; ++i ){
+        if(id == "face"+(i+1) ){ 
+            puntuacion[i] += 1;
+            total += 1;
+        document.getElementById("f"+(i+1)).innerHTML =  puntuacion[i];    
         document.getElementById("total").innerHTML = "Votos : "  + total;
+        }
     }
-    if(id=="face2"){
-        f2+= 1;  
-        total += 1;
-        document.getElementById("f2").innerHTML = f2;
-        document.getElementById("total").innerHTML = "Votos : "  + total;
-    }
-    if(id=="face3"){
-        f3 += 1; 
-        total += 1; 
-        document.getElementById("f3").innerHTML = f3;
-        document.getElementById("total").innerHTML = "Votos : "  + total;
-    }
-    if(id=="face4"){
-        total += 1;
-        f4 += 1;  
-        document.getElementById("f4").innerHTML = f4;
-        document.getElementById("total").innerHTML = "Votos : "  + total;
-    }
-    if(id=="face5"){
-        total += 1;
-        f5 += 1;  
-        document.getElementById("f5").innerHTML = f5;
-        document.getElementById("total").innerHTML = "Votos : "  + total;
-    }  
-
 }
-
